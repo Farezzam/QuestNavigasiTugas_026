fa8b20b26530a20dd42ea5b7814244075cb305a5
ASUS <faris13abiyu@gmail.com>
2025-11-09 23:59:14 +0700

    Membuat DataList

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -6,3 +6,48 @@ data class FormData(
     val status: String,
     val alamat: String
 )
+
+val datalist = listOf(
+    FormData(
+        nama = "Blecky",
+        gender = "Laki - Laki",
+        status = "Cerai",
+        alamat = "Sleman"
+    ),
+    FormData(
+        nama = "Juliana",
+        gender = "Perempuan",
+        status = "Lajang",
+        alamat = "Bantul"
+    ),
+    FormData(
+        nama = "Broddy ",
+        gender = "Laki - Laki",
+        status = "Kawin",
+        alamat = "Jogja"
+    ),
+    FormData(
+        nama = "Guz Gutsman",
+        gender = "Laki - Laki",
+        status = "Lajang",
+        alamat = "Sleman"
+    ),
+    FormData(
+        nama = "Lembud",
+        gender = "Laki - Laki",
+        status = "Lajang",
+        alamat = "Gunung Kidul"
+    ),
+    FormData(
+        nama = "Tiaraa",
+        gender = "Perempuan",
+        status = "Kawin",
+        alamat = "Banguntapan"
+    ),
+    FormData(
+        nama = "Nisa",
+        gender = "Perempuan",
+        status = "Cerai",
+        alamat = "Kasihan"
+    )
+)
