ab0cec1397f2c3d6a04974de321ae47384bc09c0
ASUS <faris13abiyu@gmail.com>
2025-11-09 23:58:09 +0700

    Membuat fun DataApp

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,7 +1,34 @@
 package com.example.navigasiku
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasiku.view.Dasboard
+
 enum class Navigasi {
     Dasboard,
     Formulir,
     TampilData
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Navigasi.Dasboard.name
+    ) {
+        composable(route = Navigasi.Dasboard.name) {
+            Dasboard(navController = navController)
+        }
+        composable(route = Navigasi.Formulir.name) {
+            Formulir(navController = navController)
+        }
+        composable(route = Navigasi.TampilData.name) {
+            TampilData(navController = navController)
+        }
+    }
 }
