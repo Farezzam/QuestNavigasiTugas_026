885afcc0c51d1ea0e7cd3d2bf3b024356f73711b
ASUS <faris13abiyu@gmail.com>
2025-11-09 23:59:42 +0700

    Membuat fun TampilData

--- app/src/main/java/com/example/navigasiku/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiku/view/TampilData.kt
@@ -1,5 +1,10 @@
 package com.example.navigasiku.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
 data class FormData(
     val nama: String,
     val gender: String,
@@ -51,3 +56,11 @@ val datalist = listOf(
         alamat = "Kasihan"
     )
 )
+
+@Composable
+fun TampilData(navController: NavHostController) {
+    val pesertaList = datalist
+
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
+    )
