4579122a02a51cf7ba1a9a6bedbf5cd14fd54ed4
ASUS <faris13abiyu@gmail.com>
2025-11-09 23:55:02 +0700

    Membuat fun Dasboard

--- app/src/main/java/com/example/navigasiku/view/Dasboard.kt
+++ app/src/main/java/com/example/navigasiku/view/Dasboard.kt
@@ -1,2 +1,12 @@
 package com.example.navigasiku.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
+@Composable
+fun Dasboard(navController: NavHostController) {
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
+    )
