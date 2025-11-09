199f4d4edde4cef7847cb53b13b05514bcfb3999
ASUS <faris13abiyu@gmail.com>
2025-11-09 23:53:40 +0700

    Menambahkan dependencies navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.navigasiku"
+    namespace = "com.example.pertemuan6"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.navigasiku"
+        applicationId = "com.example.pertemuan6"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
